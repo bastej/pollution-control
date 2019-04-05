14ab00a8ef718cc41a24a9e8432dac48ee7323aa
bastej <siatkasebastian@gmail.com>
2019-04-05 22:54:26 +0200

    cleaning the code

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
 require("./css/style.scss");
 require("./favicon.ico");
-// import $ from 'jquery';
 import map from 'lodash/map';
 
 document.addEventListener("DOMContentLoaded", function(event) {
   
   const form = document.querySelector("#country-search");
-  //init materialize accordion
+  
+  //init materialize component
   const accordion = document.querySelectorAll('.collapsible');
   const accordionInstances = M.Collapsible.init(accordion, {});
-
-  var autocomplete = document.querySelectorAll('#country-name');
-  var autocompleteInstances = M.Autocomplete.init(autocomplete,
+  const autocomplete = document.querySelectorAll('#country-name');
+  const autocompleteInstances = M.Autocomplete.init(autocomplete,
     {
       data: {
         "Poland": null,
@@ -23,35 +22,35 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
   );
 
-    form.addEventListener("submit", async e => {
-      //stop default form action
-      e.preventDefault();
-      const country = document.querySelector("#country-name").value;
+  form.addEventListener("submit", async e => {
+    //stop default form action
+    e.preventDefault();
+    const country = document.querySelector("#country-name").value;
 
-      //validate form
-      if(!validateForm(e.target)) {
-        return false
-      }
-      //when input value is correct then save to storage
-      StoreService.save("country", country);
+    //validate form
+    if(!validateForm(e.target)) {
+      return false
+    }
+    //when input value is correct then save to storage
+    StoreService.save("country", country);
 
-      UIService.clearElement(".results-header");
-      UIService.clearElement("#cities-list");
-      //show loader
-      UIService.toggleLoader("#loader");
+    UIService.clearElement(".results-header");
+    UIService.clearElement("#cities-list");
+    //show loader
+    UIService.toggleLoader("#loader");
 
-      const countrySlug = UIService.convertToSlug(country);
+    const countrySlug = UIService.convertToSlug(country);
 
-      const cities = await OpenAQService.getCities(countrySlug);
+    const cities = await OpenAQService.getCities(countrySlug);
 
-      const citiesInfo = await WikipediaService.getCitiesInfo(cities);
+    const citiesInfo = await WikipediaService.getCitiesInfo(cities);
 
-      UIService.renderCities(citiesInfo, country);
-      StoreService.get("country");
-    });
+    UIService.renderCities(citiesInfo, country);
+    StoreService.get("country");
+  });
     
-// instancje nazwac const UIService, a constructor UI
- class UI {
+
+  class UI {
 
     //render items in DOM
     renderCities(cities, country) {
@@ -97,64 +96,87 @@ document.addEventListener("DOMContentLoaded", function(event) {
         "Spain": "ES",
         "France": "FR",
       }
-      
       return slugStorage[countryName] !== undefined 
         ? slugStorage[countryName]
         : null // Default value
-    }
-
-    showAlert(text, type) {
-      const alertContainer = document.querySelector(".alert-container");
-      const alert = document.createElement("div");
-      alert.classList.add("alert", type==="success"?"green":"yellow", "lighten-4", "center-align");
-      alert.innerHTML = `<p>${text}</p>`;
-      alertContainer.appendChild(alert)
-      setTimeout(
-        () => {
-          alert.style.height = 0
-          alert.style.padding = 0
-        }
-        , 4500
-      )
     }
 
     setInputValue(input, value) {
       document.querySelector(input).value = value;
     }
 
-    toggleLocationIcon(action) {
-      const icon = document.querySelector("#location-icon");
-      action === "enable" ? icon.textContent = "my_location" : icon.textContent = "location_disabled"
-    }
-
-    // getLocation() {
-    //   if (navigator.geolocation) {
-    //     //get user location
-    //     navigator.geolocation.getCurrentPosition((position) => {
-    //       const { latitude, longitude } = position.coords;
-    //       //fetch user's country with location coords
-    //       fetch(`http://api.geonames.org/countryCode?lat=${latitude}&lng=${longitude}&type=json&username=bastej`)
-    //       .then(response => response.json())
-    //       .then(response => {
-    //         //set input value to detected country
-    //         this.toggleLocationIcon("enable");
-    //         this.setInputValue("#country-name", response.countryName)
-    //         this.showAlert("Country detected automatically", "success")
-    //       })
-    //     }, 
-    //       () => {
-    //         this.toggleLocationIcon("disable");
-    //         this.showAlert("Location is disabled, enable the location to automatically detect the country.", "warning")
-    //       }   
-    //     );
-    //   }
-    // }
-
-
   } 
   const UIService = new UI(); 
-  //get location if enabled
-  // UIService.getLocation();
+
+
+  class Store {
+
+    save(key, value) {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+    get(key) {
+      return JSON.parse(localStorage.getItem(key));
+    }
+    
+  }
+  const StoreService = new Store();
+  UIService.setInputValue("#country-name", StoreService.get("country"))
+
+
+  class Wikipedia {
+
+    //get cities descriptions
+    getCitiesInfo(citiesArr) {
+      
+      const titles = citiesArr.join("|");
+      const config = {
+        action: "query",
+        prop: "extracts",
+        exintro: true,
+        titles, 
+      }
+      return new Promise(resolve => {
+        loadJSONP(
+          "https://en.wikipedia.org/w/api.php",
+          function(data) {
+            resolve(data.query.pages);
+          },
+          config
+        );
+      })
+    }
+
+  }
+  const WikipediaService = new Wikipedia();
+
+
+  class OpenAQ {
+
+    //fetch 10 most air polluted cities
+    getCities(country) {
+      return fetch(`https://api.openaq.org/v1/latest?country=${country}&parameter=pm25`)
+      // Parse JSON response
+      .then(response => response.json())
+      // Sort from the largest by value of first measurement 
+      .then(response => response.results.sort((a, b) => b.measurements[0].value - a.measurements[0].value))
+      // Make results unique by city
+      .then(results => Object.values(
+          results.reduce((uniqueResults, result) => {
+          if (uniqueResults[result.city] === undefined) {
+              uniqueResults[result.city] = result
+          }
+          return uniqueResults
+          }, {})
+      ))
+      // Slice results to 10, because we want only 10 cities
+      .then(results => results.slice(0, 10))
+      // List results
+      .then(results => results.map(result => result.city))
+    }
+
+  }
+  const OpenAQService = new OpenAQ();
+
 
   //help to fetch data with jsonp from API that don't support CORS
   const loadJSONP = (function(){
@@ -189,22 +211,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
       document.getElementsByTagName('head')[0].appendChild(script);
     };
   })();
-
+  
   //form validation obj
   const validateForm = (function(){
     return function(form) {
-      var fields = form.querySelectorAll("[required]");
-      var errors = [];
-      var formValid = validate(errors);
+      const fields = form.querySelectorAll("[required]");
+      const errors = [];
+      const formValid = validate(errors);
       function validate(errors) {
         //IE support
-        for (var i = 0; i < fields.length; i++) {
+        for (let i = 0; i < fields.length; i++) {
           validateField(fields[i], errors);
         }
         return !errors.length;
       }
       function validateField(field, errors) {
-        var fieldValid = field.validity.valid;
+        const fieldValid = field.validity.valid;
         if (fieldValid) {
           setAsValid(field);
         } else {
@@ -231,89 +253,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }
     }
   })();
-
-  class Store {
-
-    save(key, value) {
-      localStorage.setItem(key, JSON.stringify(value));
-    }
-    get(key) {
-      return JSON.parse(localStorage.getItem(key));
-    }
-    
-  }
-  var StoreService = new Store();
-  UIService.setInputValue("#country-name", StoreService.get("country"))
-  
-
-  class Wikipedia {
-    
-    //get cities descriptions
-    getCitiesInfo(citiesArr) {
-      
-      const titles = citiesArr.join("|");
-
-        const config = {
-          action: "query",
-          prop: "extracts",
-          exintro: true,
-          titles, 
-        }
-
-        return new Promise(resolve => {
-          loadJSONP(
-            "https://en.wikipedia.org/w/api.php",
-            function(data) {
-              resolve(data.query.pages);
-            },
-            config
-          );
-
-          // or just use ajax
-          // $.ajax({
-          //   url: 'http://en.wikipedia.org/w/api.php',
-          //   data: {
-          //     action: 'query',
-          //     prop: 'info',
-          //     titles: "Katowice|Opole",
-          //     format:'json'
-          //   },
-          //   dataType:'jsonp',
-          //   success: function(data) {
-          //     console.log(data);
-          //   }
-          //   });
-
-        })
-    }
-  }
-  const WikipediaService = new Wikipedia();
-
-  class OpenAQ {
-
-    //fetch 10 most air polluted cities
-    getCities(country) {
-      return fetch(`https://api.openaq.org/v1/latest?country=${country}&parameter=pm25`)
-      // Parse JSON response
-      .then(response => response.json())
-      // Sort from the largest by value of first measurement 
-      .then(response => response.results.sort((a, b) => b.measurements[0].value - a.measurements[0].value))
-      // Make results unique by city
-      .then(results => Object.values(
-          results.reduce((uniqueResults, result) => {
-          if (uniqueResults[result.city] === undefined) {
-              uniqueResults[result.city] = result
-          }
-          return uniqueResults
-          }, {})
-      ))
-      // Slice results to 10, because we want only 10 cities
-      .then(results => results.slice(0, 10))
-      // List results
-      .then(results => results.map(result => result.city))
-    }
-
-  }
-  const OpenAQService = new OpenAQ();
 
 })
